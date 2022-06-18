26ec79fe494d443398d54f2f9239ff8bb5a76deb
Phan Thai Viet <viet.pt204895@sis.hust.edu.vn>
2022-06-18 11:47:01 +0700

    lab07 in class

--- AimsProject/src/hust/soict/dsai/aims/media/Media.java
+++ AimsProject/src/hust/soict/dsai/aims/media/Media.java
@@ -5,7 +5,7 @@ import java.util.Comparator;
 
 import hust.soict.dsai.aims.media.comparator.*;
 
-public class Media implements Comparable<Media> {
+public abstract class Media implements Comparable<Media> {
 	public static final Comparator<Media> COMPARE_BY_TITLE_COST = new MediaComparatorByTitleCost(); 
 	public static final Comparator<Media> COMPARE_BY_COST_TITLE = new MediaComparatorByCostTitle();
 	
---
+++ GUIProject/src/hust/soict/dsai/swing/NumberGrid.java
@@ -0,0 +1,35 @@
+package hust.soict.dsai.swing;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class NumberGrid extends JFrame {
+		private JButton[] btnNumbers = new JButton[10];
+		private JButton bthDelete, btnReset;
+		private JTextField tfDisplay;
+		
+		public NumberGrid() {
+			tfDisplay = new JTextField();
+			
+		}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
